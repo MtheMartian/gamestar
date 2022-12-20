1ef2f2d49fe734a53b411710b6129b11441c9d3d
MtheMartian <marvin.altidor@outlook.com>
2022-12-19 22:31:12 -0500

    Carousel is now automated.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useRef, useEffect, useState} from 'react';
+import {useRef, useEffect, useState, useMemo} from 'react';
 import '../css/homepage.css';
 import '../css/general.css';
 import xbox from '../images/xbox.png';
@@ -20,38 +20,85 @@ function Carousel(){
   // Variables
   const carousel = useRef(null);
   const [titlesCarousel, setTitlesCarousel] = useState([]);
+  const [prevBtn, setPrevBtn] = useState(null);
+  const [nextBtn, setNextBtn] = useState(null);
+  const tilesNum = useMemo(()=> titlesCarousel.length, [titlesCarousel]);
+  let tileCounter = useRef(0);
+
+  const tileTimer = useRef(null);
+
+  function setTileTimer(){
+    tileTimer.current = setInterval(()=>{
+      carousel.current.scrollLeft += carousel.current.clientWidth * 0.65;
+      tileCounter.current++;
+      console.log(tileCounter);
+      if(tileCounter.current === tilesNum){
+        carousel.current.scrollLeft = 0;
+        tileCounter.current = 0;
+      }
+    }, 9000);
+  }
+
+  function nextTile(){
+    clearInterval(tileTimer.current);
+    carousel.current.scrollLeft += carousel.current.clientWidth * 0.65;
+    tileCounter.current++;
+    console.log(tileCounter.current);
+    setTileTimer();
+    if(tileCounter.current > tilesNum){
+      tileCounter.current = 0;
+      carousel.current.scrollLeft = 0;
+      console.log(tileCounter.current);
+    }
+  }
+
+  function previousTile(){
+    clearInterval(tileTimer.current);
+    carousel.current.scrollLeft -= carousel.current.clientWidth * 0.65;
+    tileCounter.current--;
+    console.log(tileCounter.current);
+    setTileTimer();
+    if(tileCounter.current < 0){
+      tileCounter.current = 0;
+      carousel.current.scrollLeft = 0;
+      console.log(tileCounter.current);
+    }
+  }
 
   // Get titles for carousel (1 to 3 months > current date)
   useEffect(()=>{
-    let numberTitlesCarousel = 0;
     fetch("/home/carousels")
     .then(response => response.json())
     .then(data =>{
       console.log(data);
-      numberTitlesCarousel = data.length;
       setTitlesCarousel(data);
     })
     .catch(err =>{
       console.log(err);
     });
 
-    let carouselCounter = 0;
-    setInterval(()=>{
+    tileTimer.current = setInterval(()=>{
       carousel.current.scrollLeft += carousel.current.clientWidth * 0.65;
-      carouselCounter++;
-      if(carousel.current.scrollLeft === carousel.current.clientWidth * (numberTitlesCarousel - 1)
-          && carouselCounter === numberTitlesCarousel){
+      tileCounter.current++;
+      console.log(tileCounter);
+      if(tileCounter.current === tilesNum){
         carousel.current.scrollLeft = 0;
-        carouselCounter = 0;
+        tileCounter.current = 0;
       }
     }, 9000);
-  }, []);
+
+    return ()=>{
+      clearInterval(tileTimer.current);
+      console.log("lol");
+    }
+  }, [tilesNum]);
 
 
   return(
     <div id="home-page-carousel" ref={carousel}>
       {titlesCarousel.map(title =>
-        <div className="carousel-title-wrapper" key={title._id}>
+        <div className="carousel-title-wrapper" key={title._id} 
+            onMouseOver={clearInterval(tileTimer.current)}>
           <img alt="title" src={title.imgURL} className="carousel-title-imagebg" />
           <div className="carousel-title-image-wrapper">
             <img alt="title" src={title.imgURL} className="carousel-title-image" />
@@ -65,12 +112,10 @@ function Carousel(){
           <iframe src={`${title.videoURL}?controls=0&playlist=${title.videoURL.slice(30, title.videoURL.length)}&loop=1`} className="carousel-title-trailer" 
             title="Title Trailer"/>
         </div>)}
-        <button className="left" onClick={() =>{
-            carousel.current.scrollLeft -= carousel.current.clientWidth * 0.65}}>
+        <button className="left" onClick={previousTile}>
           &lt;
         </button>
-        <button className="right" onClick={() =>{
-              carousel.current.scrollLeft += carousel.current.clientWidth * 0.65}}>
+        <button className="right" onClick={nextTile}>
           &gt;
         </button>
     </div>
