ffe0dc19f98211253e2ede5365d670e458061afc
MtheMartian <marvin.altidor@outlook.com>
2023-01-06 22:22:58 -0500

    Created login page.

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignIn(){
+  return(
+    <form>
+      <input type="email" placeholder="Email"/>
+      <input type="password" placeholder="Passowrd"/>
+      <button>Sign In</button>
+    </form>
+  )
+}
